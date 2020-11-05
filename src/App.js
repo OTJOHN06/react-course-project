ac5014e3fd73d3053ffc5acb6b0617652df4b76c
OTJOHN06 <johnitater2@gmail.com>
2020-11-05 07:08:34 +0000

    made custom button and form input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop'
+import SignInAndSignOut from './pages/sign-in/sign-in'
 import Header from './components/header/header'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignOut} />
       </Switch>
     </div>
   );
